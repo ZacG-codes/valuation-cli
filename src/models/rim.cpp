f4fcfb8f5cc349591b2f6e01e2a03d7e90021392
Zac Gregory <zsgregory10@yahoo.com>
2026-09-16 00:34:36 -0500

    scratch RIM work, need to think more

--- src/models/rim.cpp
+++
@@ -1,9 +0,0 @@
-#include "models.h"
-#include <iostream>
-
-double RIM() {
-    // variables
-    double assets, liab, shares, riskFreeRate, beta, ERP, EPS, divPayoutRatio;
-    double re = riskFreeRate + (beta * ERP);
-    double RI_t = EPS_t - (re * BV_(t-1));
-}
