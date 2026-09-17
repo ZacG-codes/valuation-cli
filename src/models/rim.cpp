1ef00955d51af099962bcebc7d06c52c5835d7c8
Zac Gregory <zsgregory10@yahoo.com>
2026-09-17 15:16:42 -0500

    planning for RIM file

---
+++ src/models/rim.cpp
@@ -0,0 +1,23 @@
+#include "models.h"
+#include <iostream>
+
+double RIM() {
+    /* function prep
+     *
+     * variables needed:
+     * bvps (assets, liabilities, shares outstanding), 
+     * cost of equity (risk free rate, beta, market return),
+     * net margin,
+     * revenue,
+     * revenue growth rate,
+     * net share issuance rate,
+     * terminal growth rate, 
+     * dividend payout ratio
+     */
+
+    /* loop for forecasts */
+
+    /* equation calculation */
+
+    /* return */
+}
