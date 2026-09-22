51f35fa4c9db00b237799a741a4fe0c210297f3d
Zac Gregory <zsgregory10@yahoo.com>
2026-09-22 15:45:22 -0500

    rim logic

--- src/models/rim.cpp
+++ src/models/rim.cpp
@@ -3,18 +3,6 @@
 #include <vector>
 
 double RIM() {
-    /* function prep
-     * variables needed:
-     * bvps (assets, liabilities, shares outstanding), 
-     * cost of equity (risk free rate, beta, market return),
-     * net margin,
-     * revenue,
-     * revenue growth rate,
-     * net share issuance rate,
-     * terminal growth rate, 
-     * dividend payout ratio
-     */
-
     // raw inputs
     double assets, 
            liabilities, 
@@ -35,32 +23,48 @@ double RIM() {
     double r_e = riskFreeRate + beta * (marketReturn - riskFreeRate);
     double RI_t;
     double revenue_prior = revenue_0;
-    double net_income_0 = revenue_0 * net_margin
+    double net_income_0 = revenue_0 * net_margin;
+    double BV_prior = BV_0;
 
-    /* loop for forecasts */
-    // use vectors to hold calculated values in each forecast year
-    // i think i can use just 1 for loop, maybe? to calculate and input each value to each vector
+    // vectors for forecast values
     std::vector<double> revenue_list = {revenue_0};
     std::vector<double> net_income_list = {net_income_0};
     std::vector<double> BV_list = {BV_0};
-    std::vector<double> RI_list = {RI_0};
+    std::vector<double> RI_list;
+
+    // foreast loop
     for (int i = 1; i <= forecastYears; i++) {
         // rev
         double revenue_t = revenue_prior * revGrowthRate;
         revenue_list.push_back(revenue_t);
-        revenue_prior = revenue_t;
 
         // earnings
         double net_income_t = revenue_t * net_margin;
+        net_income_list.push_back(net_income_t);
+
+        // div
+        double dividend_t = net_income_t * dividend_payout_ratio;
 
         // BV
+        double BV_t = BV_prior + net_income_t - dividend_t;
+        BV_list.push_back(BV_t);
 
         // RI
+        double RI_t = net_income_t - (r_e * BV_prior);
+        RI_list.push_back(RI_t);
 
+        // variable updates
+        revenue_prior = revenue_t;
+        net_income_prior = net_income_t;
+        BV_prior = BV_t;
     }
 
 
     /* equation calculation */
+    double calculate() {
+
+    }
 
     /* return */
+    return value;
 }
